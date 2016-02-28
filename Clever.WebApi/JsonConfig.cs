cca8c9dbfd1a76699bf55fadca4a848e3a8d7ac8
Matthew Beatty <beattyml1@gmail.com>
2016-02-28 10:46:33 -0500

    Json config base: escape html and translate camel case

---
+++ Clever.WebApi/JsonConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Clever.WebApi
+{
+    public static class JsonConfig
+    {
+        public static void Configure()
+        {
+            var jsonFormatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.StringEscapeHandling = StringEscapeHandling.EscapeHtml;
+        }
+    }
+}
