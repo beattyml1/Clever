8dbd14a377e890e14eec7c5b26147b2a2c8105d0
Matthew Beatty <beattyml1@gmail.com>
2016-02-28 10:45:33 -0500

    Base route configs

--- Clever.WebApi/WebApiConfig.cs
+++ Clever.WebApi/WebApiConfig.cs
@@ -1,36 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Clever.WebApi;
 using System.Web.Http;
 
-namespace Clever
+namespace Clever.WebApi
 {
     public static class WebApiConfig
     {
-        public static void Register(HttpConfiguration config)
+        public static void ConfigureBaseRoutes(IdType idType, HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
+            switch (idType)
+            {
+                case IdType.Int:
+                    ConfigureBaseRoutes(config, "[0-9]+");
+                    break;
+                case IdType.GuidOrEncrypted:
+                    ConfigureBaseRoutes(config, "-.*");
+                    break;
+                case IdType.Mixed:
+                    ConfigureBaseRoutes(config, "([0-9]+)|(-.*)");
+                    break;
+            }
+        }
 
+        private static void ConfigureBaseRoutes(HttpConfiguration config, string idRegex)
+        {
             config.Routes.MapHttpRoute(
                 name: "Resource",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { },
+                constraints: new
+                {
+                    id = idRegex
+                }
             );
 
             config.Routes.MapHttpRoute(
                 name: "ResourceActions",
                 routeTemplate: "api/{controller}/{id}/{action}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { },
+                constraints: new
+                {
+                    id = idRegex,
+                    action = "[a-zA-Z][a-zA-Z0-9]*"
+                }
             );
 
             config.Routes.MapHttpRoute(
                 name: "ResourceActions",
                 routeTemplate: "api/{controller}/{action}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { },
+                constraints: new
+                {
+                    action = "[a-zA-Z][a-zA-Z0-9]*"
+                }
             );
         }
+    }
+
+    public enum IdType
+    {
+        Int, GuidOrEncrypted, Mixed
     }
 }
