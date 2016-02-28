6900f0d28d3f593c03abe8e7595906e6d3d22693
Matthew Beatty <beattyml1@gmail.com>
2016-02-28 10:44:11 -0500

    Added default dependency injection configuration

---
+++ Clever.DependencyInjection/DefaultImplmentationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clever.DependencyInjection
+{
+    [System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    sealed class DefaultImplmentationAttribute : Attribute
+    {
+        public DefaultImplmentationAttribute(Type contract, Type implmentation)
+        {
+            if (runTypes.ContainsKey(contract))
+            {
+                runTypes.Add(contract, implmentation);
+                DefaultUnityMappings.RegisterTypeDefault(contract, implmentation);
+            }
+        }
+
+        private static Dictionary<Type, Type> runTypes = new Dictionary<Type, Type>();
+    }
+}
---
+++ Clever.DependencyInjection/DefaultUnityMappings.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+
+namespace Clever.DependencyInjection
+{
+    class DefaultUnityMappings
+    {
+        private static readonly List<Action<UnityContainer>> registrations = new List<Action<UnityContainer>>();
+
+        public static void RegisterTypeDefault<TBase, TImp>() where TImp: TBase 
+        {
+            registrations.Add(container => container.RegisterType<TBase, TImp>());
+        }
+
+        public static void RegisterTypeDefault(Type tbase, Type timp)
+        {
+            registrations.Add(container => container.RegisterType(tbase, timp));
+        }
+
+        public static void RegisterDefaults(UnityContainer container)
+        {
+            registrations.ForEach(r => r(container));
+        }
+    }
+}
