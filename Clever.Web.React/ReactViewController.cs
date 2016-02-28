e3782af86b2e46d2d201c40ef9cc9f8b3e93be85
Matthew Beatty <beattyml1@gmail.com>
2016-02-28 00:13:37 -0500

    Started Work on React Stuff

---
+++ Clever.Web.React/ReactViewController.cs
@@ -0,0 +1,40 @@
+﻿using Clever.Service;
+using Clever.WebView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using React.Web;
+using React;
+using System.Web.Razor;
+using System.IO;
+
+namespace Clever.Web.React
+{
+    public class ReactViewResourceController<TId, TQuery, TIndexViewModel, TDetailViewModel, TEditorViewModel, TGetOne, TGetMany, TPut, TPost, TEditResult, TDeleteResult, TCreateResult, TSession>
+        : ViewResourceController<TId, TQuery, TIndexViewModel, TDetailViewModel, TEditorViewModel, TGetOne, TGetMany, TPut, TPost, TEditResult, TDeleteResult, TCreateResult, TSession>
+    {
+        public ReactViewResourceController(IViewService<TId, TQuery, TIndexViewModel, TDetailViewModel, TEditorViewModel, TPut, TPost, TEditResult, TDeleteResult, TCreateResult> viewService)
+            : base(viewService)
+        {
+        }
+
+        //public void DoRazorThings()
+        //{
+        //    var host = new RazorEngineHost(new CSharpRazorCodeLanguage());
+        //    host.DefaultBaseClass = typeof(int).FullName; //TODO: Get proper template base class
+            
+        //    host.DefaultNamespace = "RazorOutput";
+        //    host.DefaultClassName = "Template";
+            
+        //    host.NamespaceImports.Add("System");
+        //    host.NamespaceImports.Add("System.IO");
+
+        //    var razorEngine = new RazorTemplateEngine(host);
+            
+        //    razorEngine.ParseTemplate(new StringReader("")).Document.
+        //}
+    }
+}
